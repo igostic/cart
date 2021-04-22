0459a32383b2cfd255bbb663c1a4a7aff39e24f6
Prakhar Chauhan <shinuc29@gmail.com>
2021-04-22 18:41:31 +0530

    Add cartItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import CartItem from './CartItem';
 function App() {
   return (
     <div className="App">
-    <h1>CART</h1>
+    <h1>Shopping Cart</h1>
+    <CartItem />
     </div>
   );
 }
---
+++ src/CartItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class CartItem extends React.Component {
+    render() {
+        return (
+            <div className="cart-item">
+                <div className="left-block">
+                    <img style = {styles.image} alt=""/>
+                </div>
+
+                <div className="right-block">
+                   <div style = { { fontSize: 25 } }> Phones</div>
+                   <div style = { { color: '#777' } }> Rs 999</div>
+                   <div style = { { color: '#777' } }> Qty: 1</div>
+                   <div ClassName="cart-item-actions">
+                       {/* {Buttons} */}
+                   </div>
+                </div>
+            </div>
+        );   
+    }
+}
+
+const styles = {
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        background : '#777'
+    }
+}
+
+export default CartItem;
